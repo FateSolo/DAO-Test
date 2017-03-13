9ffd5271fb75aa02eff72542b0260005d394fd52
FateSolo <FateSolo@mail.bistu.edu.cn>
2017-03-13 13:25:54 +0800

    三：Mybatis框架

---
+++ src/main/java/com/fatesolo/dao/impl/BookDaoMybatisImpl.java
@@ -0,0 +1,8 @@
+package com.fatesolo.dao.impl;
+
+import com.fatesolo.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+@Repository("bookDaoMybatisImpl")
+public interface BookDaoMybatisImpl extends BookDao {
+}
