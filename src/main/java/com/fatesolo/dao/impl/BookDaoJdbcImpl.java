cc9aed533ad86cdfb52b19e97e360ebc8997cbae
FateSolo <FateSolo@mail.bistu.edu.cn>
2017-05-08 10:03:17 +0800

    try-with-resources

--- src/main/java/com/fatesolo/dao/impl/BookDaoJdbcImpl.java
+++ src/main/java/com/fatesolo/dao/impl/BookDaoJdbcImpl.java
@@ -18,48 +18,24 @@ public class BookDaoJdbcImpl implements BookDao {
 
     @Override
     public Book findById(int id) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-
         String sql = "select name, author from book where id = ?";
 
-        try {
-            connection = dataSource.getConnection();
-
-            statement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
 
-            rs = statement.executeQuery();
-            if (rs.next()) {
-                Book book = new Book();
-                book.setId(id);
-                book.setName(rs.getString("name"));
-                book.setAuthor(rs.getString("author"));
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    Book book = new Book();
+                    book.setId(id);
+                    book.setName(rs.getString("name"));
+                    book.setAuthor(rs.getString("author"));
 
-                return book;
+                    return book;
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignored) {
-                }
-            }
         }
 
         return null;
@@ -67,50 +43,26 @@ public class BookDaoJdbcImpl implements BookDao {
 
     @Override
     public List<Book> findByNameContaining(String name) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-
         String sql = "select id, name, author from book where name like ?";
 
         List<Book> books = new ArrayList<>();
 
-        try {
-            connection = dataSource.getConnection();
-
-            statement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, "%" + name + "%");
 
-            rs = statement.executeQuery();
-            while (rs.next()) {
-                Book book = new Book();
-                book.setId(rs.getInt("id"));
-                book.setName(rs.getString("name"));
-                book.setAuthor(rs.getString("author"));
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    Book book = new Book();
+                    book.setId(rs.getInt("id"));
+                    book.setName(rs.getString("name"));
+                    book.setAuthor(rs.getString("author"));
 
-                books.add(book);
+                    books.add(book);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignored) {
-                }
-            }
         }
 
         return books;
@@ -118,46 +70,22 @@ public class BookDaoJdbcImpl implements BookDao {
 
     @Override
     public void save(Book book) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-
         String sql = "insert into book(name, author) values(?, ?)";
 
-        try {
-            connection = dataSource.getConnection();
-
-            statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, book.getName());
             statement.setString(2, book.getAuthor());
 
             statement.executeUpdate();
 
-            rs = statement.getGeneratedKeys();
-            if (rs.next()) {
-                book.setId(rs.getInt(1));
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                if (rs.next()) {
+                    book.setId(rs.getInt(1));
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException ignored) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignored) {
-                }
-            }
         }
     }
 
